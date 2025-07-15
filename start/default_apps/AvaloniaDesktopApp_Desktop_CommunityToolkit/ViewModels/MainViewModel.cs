7cb1441cc0f6489c082ebaebde8c57712b4a0a87
Ilya Fralkou <dzmuh@outlook.com>
2025-07-15 04:07:26 +0200

    Avalonia 11.2.5, NET 9,0

--- start/default_apps/AvaloniaDesktopApp_Desktop_CommunityToolkit/ViewModels/MainViewModel.cs
+++ start/default_apps/AvaloniaDesktopApp_Desktop_CommunityToolkit/ViewModels/MainViewModel.cs
@@ -1,6 +1,15 @@
-﻿namespace AvaloniaDesktopApp_Desktop_CommunityToolkit.ViewModels;
+﻿using System.Reflection;
+using Avalonia;
+
+namespace AvaloniaDesktopApp_Desktop_CommunityToolkit.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
-    public string Greeting => "Welcome to Avalonia!";
+    public string AvaloniaVersion => Assembly.GetAssembly(typeof(Avalonia.Controls.Button)).GetName().Version.ToString();
+
+    // Retrieves the current .NET version
+    public string DotNetVersion = System.Environment.Version.ToString();
+
+    public string Greeting => ($"Welcome to Avalonia {AvaloniaVersion} and .NET Core {DotNetVersion}!");
 }
+ 
--- start/default_apps/AvaloniaDesktopApp_ReactiveUI/ViewModels/MainViewModel.cs
+++ start/default_apps/AvaloniaDesktopApp_ReactiveUI/ViewModels/MainViewModel.cs
@@ -1,6 +1,14 @@
-﻿namespace AvaloniaDesktopApp_ReactiveUI.ViewModels;
+﻿using System.Reflection;
+using Avalonia;
+
+namespace AvaloniaDesktopApp_ReactiveUI.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
-    public string Greeting => "Welcome to Avalonia!";
+    public string AvaloniaVersion => Assembly.GetAssembly(typeof(Avalonia.Controls.Button)).GetName().Version.ToString();
+
+    // Retrieves the current .NET version
+    public string DotNetVersion = System.Environment.Version.ToString();
+
+    public string Greeting => ($"Welcome to Avalonia {AvaloniaVersion} and .NET Core {DotNetVersion}!");
 }
